64872c96a6cd6fabf2b2717e20c4570ea5a1dc94
Fahima <fahimakaz2@gmail.com>
2025-11-26 11:30:16 +0530

    Add vowel or consonant checker program

---
+++ Q2.c
@@ -0,0 +1,25 @@
+//to check whether a vowel or consonant
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char ch;
+    printf("enter any alphabet you want to check:");
+    scanf("%c",&ch);
+    if (isalpha(ch))
+    {
+        ch=tolower(ch);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        {
+            printf("It's a vowel");
+
+        }
+        else
+            printf("It's a constant");
+    }
+
+    else
+        printf("ERROR!!!! \n Enter an alphabet please......");
+
+    return 0;
+}
