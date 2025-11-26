786955ae21bb5fd0d63cfddd7be345db2879ebf3
Fahima <fahimakaz2@gmail.com>
2025-11-26 10:57:18 +0530

    q1.c

--- q1.c
+++ q1.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-  printf("Jamia!Jamia!Jamia\n");
-  printf("\"Jamia!Jamia!\\\"");
+  printf("Jamia!Jamia!Jamia\\n\n");
+  printf("\"Jamia!Jamia!\\\"\n");
   printf("Jamia!%%");
   return 0;
 }
